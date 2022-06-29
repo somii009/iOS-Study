de09a96b90427e25b697cd9c6c2943d8a772c0d4
Somi <rlathal009@gmail.com>
2022-06-29 23:12:14 +0900

    I am Rich Project

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Somi Kim on 2022/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
