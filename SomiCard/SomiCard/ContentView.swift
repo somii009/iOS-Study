7926795c14d785d6862c4328bb62a0414b21aec0
Somi <rlathal009@gmail.com>
2022-08-22 22:55:41 +0900

    Card Exercise : Initial commit

---
+++ SomiCard/SomiCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SomiCard
+//
+//  Created by Somi Kim on 2022/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SomiCard/SomiCard/SomiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomiCardApp.swift
+//  SomiCard
+//
+//  Created by Somi Kim on 2022/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SomiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
