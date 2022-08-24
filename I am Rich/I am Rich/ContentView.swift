46efb4343ddbdf32d4e1b087f9b3937200347c3c
Somi <rlathal009@gmail.com>
2022-08-24 23:14:07 +0900

    Card Exercise

--- I am Rich/I am Rich/ContentView.swift
+++ I am Rich/I am Rich/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+            
+        }
+        
     }
 }
 
